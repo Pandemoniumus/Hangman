06b1e11ecd08abec8bb65af2b1b1425a59269da3
Pandemoniumus <maximus_100@mail.ru>
2025-01-22 02:33:41 +0300

    Updated and optimized functions in accordance with 2nd review from Sergey U. (@grandpraline). Literals and resources were separated from main module. The code has become more clear and pythony ;)

--- hangman.py
+++ hangman.py
@@ -1,176 +1,183 @@
 import re
 from random import choice
+from resources import hangman, mask_sign, separator
+from messages import USER_MESSAGES
 
-def game():
-    game_session = 0
 
-    # Генератор нового слова
-    def get_new_word():
-        file_url = 'russian-nouns.txt'
+attempts_quantity = len(hangman) - 2    # количество попыток в игре в зависимости от количества состояний виселицы
+
+def get_new_word(file_url: str = 'russian-nouns.txt') -> str | None:
+    """
+        Возвращает случайное слово из файла.
+
+        :param file_url: Путь и название файла со словами
+        :return: Слово в верхнем регистре или None, если файл отсутствует
+        """
+    try:
+        with open(file_url, 'r', encoding='utf-8') as file:
+            words = [word.strip() for word in file if word.strip()]
+            if not words:           # проверка на пустой файл
+                raise FileNotFoundError
+            new_word = choice(words)
+            return new_word.upper()
+    except FileNotFoundError:
+        raise FileNotFoundError('Упс, кажется я не могу придумать ни одного слова. '
+                                'Файл со словами потерялся или поврежден. Пожалуйста, свяжитесь с разработчиком.')
+
+
+def show_guessed_word(guessed_word: str) -> str:
+    """
+    Форматирует и возвращает сообщение с загаданным словом.
+
+    :param guessed_word: Загаданное слово, которое будет выведено.
+    :return: Строка с сообщением о загаданном слове.
+    """
+    return f"Загаданное слово: {guessed_word}"
+
+
+def get_hangman_status(key: int | str = 0) -> str:
+    """
+    Возвращает графическое отображение виселицы в зависимости от количества ошибок или поздравление с победой.
+
+    :param key: Количество ошибок (целое число от 0 до количества состояний виселицы) или слово 'win' для отображения победного состояния.
+    :return: Строка с ASCII-символами, представляющими состояние виселицы или поздравление с победой.
+    """
+
+    return ''.join(hangman[key])
+
+
+def update_masked_word(guessed_word: str, inputted_letter: str, masked_word: str) -> str:
+    """
+    Проверяет наличие введенной буквы в загаданном слове, открывает ее в маске слова и возвращает обновленную маску слова.
+
+    :param guessed_word: Загаданное слово, в котором нужно искать букву.
+    :param inputted_letter: Введенная пользователем буква, которую нужно проверить.
+    :param masked_word: Текущее состояние слова с угаданными буквами (маска).
+    :return: Обновленное состояние слова с открытой(ыми) угаданной(ыми) буквой(ами).
+    """
+    result_word = masked_word
+    if inputted_letter in guessed_word:     #TODO: уточнить необходимость данной проверки и исключить при ненадобности
+        for i in range(len(guessed_word)):
+            if guessed_word[i] == inputted_letter:
+                result_word = result_word[:i] + inputted_letter + result_word[i + 1:]
+    return result_word
+
+
+def start_or_quit_game() -> bool:
+    """
+    Предлагает игроку выбрать между началом новой игры или выходом.
+
+    :return: True - если игрок выбрал начать новую игру; False - если игрок выбрал выход.
+    """
+    while True:
         try:
-            with open(file_url, 'r', encoding='utf-8') as file:
-                words = file.readlines()
-                new_word = choice(words).strip()
-                return new_word.upper()
-        except FileNotFoundError:
-            raise FileNotFoundError('Упс, кажется я не могу придумать ни одного слова. '
-                  'Файл со словами потерялся или поврежден. Пожалуйста, свяжитесь с разработчиком.')
+            player_choice = (input(f"Для начала новой игры введите цифру 1"
+                                   f"\nДля выхода из игры введите цифру 0"
+                                   f"\n"))
+            match player_choice:
+                case "1":
+                    return True
+                case "0":
+                    print(USER_MESSAGES['GAME_OVER'])
+                    return False
+                case _:
+                    raise ValueError
+        except ValueError:
+            print("Вы ввели некорректные данные.")
 
-    # Описание и графический вывод Виселицы
-    def get_hangman_status(key: int|str = 0):
-        hangman = {
-            0: [
-                f"\033[1m{'_':2}\033[0m" * 4,
-                f"\n\033[1m{chr(124):6}\033[0m{chr(166)}",
-                f"\n\033[1m{chr(124):6}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[0m",
-                f"\n\033[1m{chr(124)}\033[0m{chr(46) * 8}\n"
-            ],
-            1: [
-                f"\033[1m{'_':2}\033[0m" * 4,
-                f"\n\033[1m{chr(124):6}\033[0m{chr(166)}",
-                f"\n\033[1m{chr(124):6}\033[33m{chr(111)}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[0m",
-                f"\n\033[1m{chr(124)}\033[0m{chr(46) * 8}\n"
-            ],
-            2: [
-                f"\033[1m{'_':2}\033[0m" * 4,
-                f"\n\033[1m{chr(124):6}\033[0m{chr(166)}",
-                f"\n\033[1m{chr(124):6}\033[33m{chr(111)}\033[0m",
-                f"\n\033[1m{chr(124):6}\033[33m{chr(124)}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[0m",
-                f"\n\033[1m{chr(124)}\033[0m{chr(46) * 8}\n"
-            ],
-            3: [
-                f"\033[1m{'_':2}\033[0m" * 4,
-                f"\n\033[1m{chr(124):6}\033[0m{chr(166)}",
-                f"\n\033[1m{chr(124):6}\033[33m{chr(111)}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[33m{chr(47)}{chr(124)}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[0m",
-                f"\n\033[1m{chr(124)}\033[0m{chr(46) * 8}\n"
-            ],
-            4: [
-                f"\033[1m{'_':2}\033[0m" * 4,
-                f"\n\033[1m{chr(124):6}\033[0m{chr(166)}",
-                f"\n\033[1m{chr(124):6}\033[33m{chr(111)}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[33m{chr(47)}{chr(124)}{chr(92)}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[0m",
-                f"\n\033[1m{chr(124)}\033[0m{chr(46) * 8}\n"
-            ],
-            5: [
-                f"\033[1m{'_':2}\033[0m" * 4,
-                f"\n\033[1m{chr(124):6}\033[0m{chr(166)}",
-                f"\n\033[1m{chr(124):6}\033[33m{chr(111)}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[33m{chr(47)}{chr(124)}{chr(92)}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[33m{chr(47):2}\033[0m",
-                f"\n\033[1m{chr(124)}\033[0m{chr(46) * 8}\n"
-            ],
-            6: [
-                f"\033[1m{'_':2}\033[0m" * 4,
-                f"\n\033[1m{chr(124):6}\033[0m{chr(166)}",
-                f"\n\033[1m{chr(124):6}\033[31m{chr(111)}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[31m{chr(47)}{chr(124)}{chr(92)}\033[0m",
-                f"\n\033[1m{chr(124):5}\033[31m{chr(47):2}{chr(92)}\033[0m",
-                f"\n\033[1m{chr(124)}\033[0m{chr(46) * 8}\n"
-            ],
-            'win': [
-                '(\\___/)',
-                f"\n(='.'=)",
-                f'\n(")_(")'
-            ]
-        }
-        return ''.join(hangman[key])
 
-    # Проверка введенной буквы в слове
-    def check_letter(word: str, letter, masked_word: str):
-        result_word = masked_word
-        if letter in word:
-            for i in range(len(word)):
-                if word[i] == letter:
-                    result_word = result_word[:i] + letter + result_word[i+1:]
-        return result_word
+def is_cyrillic_char(inputted_letter: str) -> bool:
+    """
+    Проверяет, является ли введённый символ одной русской буквой (заглавной).
 
-    # Меню (игра или выход)
-    def start_or_exit_menu():
-        while True:
-            try:
-                player_choice = (input(f"Для начала новой игры введите цифру 1"
-                                          f"\nДля выхода из игры введите цифру 0"
-                                          f"\n"))
-                match player_choice:
-                    case "1":
-                        return True
-                    case "0":
-                        print('КОНЕЦ ИГРЫ')
-                        return False
-                    case _:
-                        raise ValueError
-            except ValueError as e:
-                print("Вы ввели некорректные данные.")
+    :param inputted_letter: Буква для проверки.
+    :return: True, если символ соответствует одной заглавной русской букве, иначе False.
+    """
 
-    # Запуск игры
-    def start():
-        nonlocal game_session
-        # проверяем, первый ли запуск игры пользователем (session) - для вывода приветствия
-        if game_session == 0:
-            print(f'Привет, искатель приключений!')
-            print('Это игра ВИСЕЛИЦА! Твоя задача отгадать задуманное мной слово по буквам. Поехали?')
-            print('~' * 20)
+    return bool(re.fullmatch(r'[А-ЯЁ]', inputted_letter))
 
-        while start_or_exit_menu():
 
-            try:
-                new_word = get_new_word()
-            except FileNotFoundError as e:
-                print(e)
-                return
-            masked_word = '_' * len(new_word)
-            wrong_letters = []
-            faults = 0
+def show_game_result(masked_word: str, new_word: str, faults: int) -> None:
+    """
+    Отображает результат игры (победа или поражение).
 
-            while faults < 6:
+    :param masked_word: Текущее состояние слова с угаданными буквами (маска).
+    :param new_word: Загаданное слово.
+    :param faults: Количество ошибок, допущенных игроком.
+    :return: None
+    """
+    if mask_sign not in masked_word:
+        # Победа
+        print(get_hangman_status('win'),
+              USER_MESSAGES['WIN'], show_guessed_word(new_word),
+              sep='\n')
+    else:
+        # Поражение
+        print(get_hangman_status(faults),
+              USER_MESSAGES['LOSS'] + ' ' + show_guessed_word(new_word),
+              sep='\n')
 
-                print(get_hangman_status(faults))
-                print('СЛОВО: ', ' '.join(masked_word))
+    # Разделитель для оформления вывода
+    print(separator['tilda'])
 
-                # Проверка на вводимые символы (должна быть только 1 русская буква)
-                is_cyr_char = lambda char: bool(re.fullmatch(r'[А-ЯЁ]', char))
-                while True:
-                    current_letter = input('Введите одну русскую букву: ').upper()
 
-                    if is_cyr_char(current_letter):
-                        if not current_letter in new_word and not current_letter in wrong_letters:
-                            faults += 1
-                            wrong_letters.append(current_letter)
-                            print(f"Такой буквы в слове нет.")
-                        else:
-                            masked_word = check_letter(new_word, current_letter, masked_word)
-                        break
+def start_game() -> None:
+    """
+    Запускает игру 'Виселица' с основным игровым циклом.
+
+    Последовательность действий:
+    - Приветствие игрока.
+    - Выбор начала новой игры или выхода через меню.
+    - Считывание случайного слова из файла.
+    - Основной игровой процесс с вводом букв, проверкой слова и отображением состояния виселицы.
+    - Вывод результата (победа или поражение).
+
+    :return: None
+    """
+
+    print(USER_MESSAGES['GREETING'])
+
+    while start_or_quit_game():
+        try:
+            guessed_word = get_new_word()
+        except FileNotFoundError as e:
+            print(e)
+            return
+        masked_word = mask_sign * len(guessed_word)
+        wrong_letters = []
+        faults = 0
+
+        while faults < attempts_quantity:
+
+            # Выводим текущий статус виселицы и загаданного слова
+            print(get_hangman_status(faults))
+            print('СЛОВО: ', ' '.join(masked_word))
+
+            while True:
+                inputted_letter = input('Введите одну русскую букву: ').upper()
+
+                if is_cyrillic_char(inputted_letter):
+                    if not inputted_letter in guessed_word and not inputted_letter in wrong_letters:
+                        faults += 1
+                        wrong_letters.append(inputted_letter)
+                        print(f"Такой буквы в слове нет.")
                     else:
-                        print('Вы ввели некорректные данные!')
-
-                # Выводим количество ошибок и вводимые неправильные буквы
-                print(f'ОШИБКИ: ({faults} из 6) \033[31m{", ".join(wrong_letters) if wrong_letters else ''}\033[0m')
-                print(f'+ ' * 20)
-
-                # Проверка на выход из цикла в случае, если Пользователь угадал слово целиком
-                if not '_' in masked_word:
+                        masked_word = update_masked_word(guessed_word, inputted_letter, masked_word)
                     break
+                else:
+                    print('Вы ввели некорректные данные!')
 
-            # Выводим итоговую виселицу и результат
-            if not '_' in masked_word:
-                print(get_hangman_status('win'),
-                      f'Вы выиграли, поздравляю! Загаданное слово - {new_word}', sep='\n')
-            else:
-                print(get_hangman_status(faults),
-                      f'Вы проиграли. Загаданное слово - {new_word}', sep='\n')
-            print('~' * 20)
+            # Выводим количество ошибок и вводимые неправильные буквы
+            print(f'ОШИБКИ: ({faults} из {attempts_quantity}) \033[31m{", ".join(sorted(wrong_letters)) if wrong_letters else ''}\033[0m')
+            # \033[31m - красный цвет, \033[0m - сброс цвета
+            print(separator['plus'])
 
-            game_session += 1
-            # start()     # Предлагаем сыграть еще или выйти из игры
+            # Проверка на выход из цикла в случае, если Пользователь угадал слово целиком
+            if not mask_sign in masked_word:
+                break
 
-    return start()
+        show_game_result(masked_word, guessed_word, faults)
 
-game()
+
+if __name__ == '__main__':
+    start_game()
